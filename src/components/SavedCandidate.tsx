c9bad78504b5ee2c20bce7c36b88c04e6fb0376f
David McCullough <dmccullough488@gmail.com>
2024-09-30 23:36:40 -0500

    Start of saved candidate component

--- src/components/SavedCandidate.tsx
+++ src/components/SavedCandidate.tsx
@@ -1,6 +1,37 @@
 import type { Candidate } from "../interfaces/Candidate.interface";
 import { IoRemoveCircle } from "react-icons/io5";
 
+type SavedCandidateProps = {
+  candidate: Candidate;
+  rejectCandidate: (id: number) => void;
+};
+
+const SavedCandidate = ({
+  candidate,
+  rejectCandidate,
+}: SavedCandidateProps) => {
+  return (
+    <tr>
+      {candidate ? (
+        <>
+          <td>
+            <img
+              src={`${candidate.avatar_url}`}
+              alt={`Profile of ${candidate.login}`}
+              style={{
+                width: '60px',
+                borderRadius: '15px',
+                display: 'block',
+                margin: '0 auto',
+              }}
+            />
+          </td>
+        </>
+      )}
+    </tr>
+  )
+}
+
 
 
 
